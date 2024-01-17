0207a679d59abf23ac538dd485897427188fbd3f
markkorenevskiy <mark.korenevskiy@gmail.com>
2024-01-17 12:27:50 -0500

    lab_01 done

--- Lab01_101301087_COMP3133/index.js
+++ Lab01_101301087_COMP3133/index.js
@@ -1,3 +1,24 @@
 'use strict'
 
+import fs from 'fs';
+import csvParser from "csv-parser";
 
+function main() {
+    ['canada.txt', 'usa.txt'].forEach(file => {
+        if (fs.existsSync(file)) {
+            fs.unlinkSync(file);
+        }
+    });
+
+    fs.createReadStream('input_countries.csv')
+        .pipe(csvParser())
+        .on('data', (row) => {
+            if (row.country === 'Canada') {
+                fs.appendFileSync('canada.txt', `${row.country},${row.year},${row.population}\n`);
+            } else if (row.country === 'United States') {
+                fs.appendFileSync('usa.txt', `${row.country},${row.year},${row.population}\n`);
+            }
+        });
+}
+
+main();
